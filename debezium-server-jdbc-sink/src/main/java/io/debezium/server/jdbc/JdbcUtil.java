c6b20e7620300e260d8cbebc64da630d18475c98
Ismail Simsek <6005685+ismailsimsek@users.noreply.github.com>
2023-01-31 21:31:03 +0100

    Minor simplify deduplicateBatch method

--- debezium-server-jdbc-sink/src/main/java/io/debezium/server/jdbc/JdbcUtil.java
+++ debezium-server-jdbc-sink/src/main/java/io/debezium/server/jdbc/JdbcUtil.java
@@ -9,20 +9,20 @@
 package io.debezium.server.jdbc;
 
 import io.debezium.DebeziumException;
+import io.debezium.server.jdbc.querydsl.ddl.CreateTableClause;
 
 import java.sql.Connection;
-import java.util.ArrayList;
+import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import javax.enterprise.inject.Instance;
 import javax.enterprise.inject.literal.NamedLiteral;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.querydsl.sql.Configuration;
+import com.querydsl.sql.SQLTemplatesRegistry;
 import org.eclipse.microprofile.config.Config;
-import org.jooq.*;
-import org.jooq.impl.DSL;
-import org.jooq.impl.SQLDataType;
+import org.jdbi.v3.core.qualifier.QualifiedType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -55,15 +55,15 @@ public class JdbcUtil {
     return ret;
   }
 
-  private static Map<String, DataType<?>> fields(JsonNode valueSchema) {
+  private static Map<String, Class<?>> fieldsV2(JsonNode valueSchema) {
     if (valueSchema != null && valueSchema.has("fields") && valueSchema.get("fields").isArray()) {
-      return fields(valueSchema, "", 0);
+      return fieldsV2(valueSchema, "", 0);
     }
     return new HashMap<>();
   }
 
-  private static Map<String, DataType<?>> fields(JsonNode eventSchema, String schemaName, int columnId) {
-    Map<String, DataType<?>> fields = new HashMap<>();
+  private static Map<String, Class<?>> fieldsV2(JsonNode eventSchema, String schemaName, int columnId) {
+    Map<String, Class<?>> fields = new HashMap<>();
     String schemaType = eventSchema.get("type").textValue();
     LOGGER.debug("Converting Schema of: {}::{}", schemaName, schemaType);
     for (JsonNode jsonSchemaFieldNode : eventSchema.get("fields")) {
@@ -75,7 +75,7 @@ public class JdbcUtil {
         case "array":
           JsonNode items = jsonSchemaFieldNode.get("items");
           if (items != null && items.has("type")) {
-            fields.put(fieldName, SQLDataType.LONGNVARCHAR);
+            fields.put(fieldName, String.class);// java.sql.Array
 //              String listItemType = items.get("type").textValue();
 //
 //              if (listItemType.equals("struct") || listItemType.equals("array") || listItemType.equals("map")) {
@@ -88,18 +88,18 @@ public class JdbcUtil {
           }
           break;
         case "map":
-          fields.put(fieldName, SQLDataType.LONGNVARCHAR);
+          fields.put(fieldName, String.class);
           //fields.put(fieldName, QualifiedType.of(Map.class));
           //throw new RuntimeException("'" + fieldName + "' has Map type, Map type not supported!");
           break;
         case "struct":
           // create it as struct, nested type
-          fields.put(fieldName, SQLDataType.LONGNVARCHAR);
+          fields.put(fieldName, String.class);
           //Map<String, QualifiedType<?>> subSchema = fields(jsonSchemaFieldNode, fieldName, columnId);
           //fields.put(fieldName, subSchema);
           break;
         default: //primitive types
-          fields.put(fieldName, fieldType(fieldType));
+          fields.put(fieldName, fieldTypeV2(fieldType));
           break;
       }
     }
@@ -107,57 +107,56 @@ public class JdbcUtil {
     return fields;
   }
 
-  private static DataType<?> fieldType(String fieldType) {
+  private static Class<?> fieldTypeV2(String fieldType) {
     switch (fieldType) {
       case "int8":
       case "int16":
       case "int32": // int 4 bytes
-        return SQLDataType.BIGINT;
+        return long.class;
       case "int64": // long 8 bytes
-        return SQLDataType.BIGINT;
+        return long.class;
       case "float8":
       case "float16":
       case "float32": // float is represented in 32 bits,
-        return SQLDataType.DOUBLE;
+        return double.class;
       case "float64": // double is represented in 64 bits
-        return SQLDataType.DOUBLE;
+        return double.class;
       case "boolean":
-        return SQLDataType.BOOLEAN;
+        return boolean.class;
       case "string":
-        return SQLDataType.LONGNVARCHAR;
+        return String.class;
       case "uuid":
-        return SQLDataType.LONGNVARCHAR;
+        return String.class;
       case "bytes":
-        return SQLDataType.BINARY;
+        return byte.class;
       default:
         // default to String type
-        return SQLDataType.LONGNVARCHAR;
+        return String.class;
       //throw new RuntimeException("'" + fieldName + "' has "+fieldType+" type, "+fieldType+" not supported!");
     }
   }
 
+
   public static void createTable(String schemaName, String tableName, Connection conn, JdbcChangeEvent.Schema schema,
                                  boolean upsert) {
-    DSLContext create = DSL.using(conn);
-    Map<String, DataType<?>> fl = JdbcUtil.fields(schema.valueSchema());
-    List<Field<?>> fields = new ArrayList<>();
+    try {
+      SQLTemplatesRegistry tr = new SQLTemplatesRegistry();
+      Configuration configuration = new Configuration(tr.getTemplates(conn.getMetaData()));
+      String tableNameWithSchema = String.format("%s.%s", schemaName, tableName);
+      CreateTableClause ct = new CreateTableClause(conn, configuration, tableNameWithSchema);
 
-    fl.forEach((k, v) -> fields.add(DSL.field(DSL.name(k), v)));
-    create.setSchema(schemaName);
+      Map<String, Class<?>> fieldv2 = JdbcUtil.fieldsV2(schema.valueSchema());
 
-    if (upsert && schema.keySchema() != null) {
-      try (CreateTableConstraintStep sql = create.createTable(tableName)
-          .columns(fields)
-          .primaryKey(schema.keySchemaFields().keySet().toArray(new String[0]))) {
-        LOGGER.warn("Creating table:\n{}", sql.getSQL());
-        sql.execute();
+      fieldv2.forEach(ct::column);
+
+      Map<String, QualifiedType<?>> SchemaPKFields = schema.keySchemaFields();
+      if (!SchemaPKFields.isEmpty()) {
+        ct.primaryKey(String.format("%s", tableName), SchemaPKFields.keySet().toArray(new String[0]));
       }
-    } else {
-      try (CreateTableColumnStep sql = create.createTable(tableName)
-          .columns(fields)) {
-        LOGGER.warn("Creating table:\n{}", sql.getSQL());
-        sql.execute();
-      }
+
+      ct.execute();
+    } catch (SQLException e) {
+      throw new DebeziumException("Failed to create table", e);
     }
   }
 
